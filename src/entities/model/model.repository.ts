4e2b17399103f3104e7b4542c7d417426f30a845
Ubuntu <ubuntu@ip-172-31-23-32.ap-southeast-1.compute.internal>
2023-04-11 08:37:08 +0000

    add ceramic model entity

--- src/entities/model/model.repository.ts
+++ src/entities/model/model.repository.ts
@@ -1,8 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { MetaModel, MetaModelMainnet } from './model.entity';
+import { CeramicModelTestNet, MetaModel, MetaModelMainnet } from './model.entity';
 
 @EntityRepository(MetaModel)
 export class MetaModelRepository extends Repository<MetaModel> {}
 
 @EntityRepository(MetaModelMainnet)
 export class MetaModelMainnetRepository extends Repository<MetaModelMainnet> {}
+
+@EntityRepository(CeramicModelTestNet)
+export class CeramicModelTestNetRepository extends Repository<CeramicModelTestNet> {}
