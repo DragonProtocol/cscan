add89fa000caca1074429ebcb2ea2c4e14dbaa61
Ubuntu <ubuntu@ip-172-31-23-32.ap-southeast-1.compute.internal>
2023-06-07 07:57:30 +0000

    add some code for parser

--- src/utils/graphql/parser.ts
+++ src/utils/graphql/parser.ts
@@ -1,10 +1,19 @@
-import GraphQLLanguage from 'graphql/language';
+import { parse } from 'graphql/language';
 
-export function parseToModelGraphqls(grapql: string): [] {
-    if(grapql) {
-        const ast = GraphQLLanguage.parse(grapql);
-        const definitions = ast.definitions;
-        return definitions;
-    }
+export function parseToModelGraphqls(graphql: string): [] {
+    if (!graphql || graphql.length == 0) return []
+
+    const ast = parse(graphql);
+    if (!ast || ast.definitions.length == 0) return [];
+    // checkout definitions of createModel derectives
+
+    // generate  EnumTypeDefinition/ObjectTypeDefinition map
+
+
+    // const definitions = ast.definitions;
+    // return definitions.map((definition: any) => {
+    //     const name = definition.name.value;
+    //     return name
+    // });
     return [];
 }
